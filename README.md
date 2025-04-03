6e0560a4eed9914051e1d94e14fb2b36538bae12
Nick F <isnotnick@users.noreply.github.com>
2025-04-03 10:15:19 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pemdecoder
+Simple Go PEM-blob decoder
+--
+
+Takes a PEM-encoded blob and decodes it. Can function even without the usual PEM headers (by trying various possibilties) and outputs Go-stdlib format parsed data.
---
+++ pemdecoder.go
@@ -0,0 +1,102 @@
+package pemdecoder
+
+import (
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
+)
+
+// make struct 'type' that holds a shorthand 'type' (cert, crl, csr, key, p7?) and the JSON data so a front-end can format the data nicely
+
+func decodePEM(pemBlock string) (string, error) {
+	//	First, attempt to decode the PEM and let Go handle the type
+	var result interface{}
+
+	block, _ := pem.Decode([]byte(pemBlock))
+
+	if block == nil {
+		//	If it fails, let's try some common types by adding headers and try the specidfic parsing
+		block, _ = pem.Decode([]byte("\n-----BEGIN CERTIFICATE REQUEST-----\n" + pemBlock + "\n-----END CERTIFICATE REQUEST-----\n"))
+		if block != nil {
+			csr, err := x509.ParseCertificateRequest(block.Bytes)
+
+			if err != nil {
+				block, _ = pem.Decode([]byte("-----BEGIN CERTIFICATE-----\n" + pemBlock + "\n-----END CERTIFICATE-----\n"))
+				if block != nil {
+					cert, err := x509.ParseCertificate(block.Bytes)
+
+					if err != nil {
+						block, _ = pem.Decode([]byte("\n-----BEGIN PRIVATE KEY-----\n" + pemBlock + "\n-----END PRIVATE KEY-----\n"))
+
+						if block != nil {
+							key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+							if err != nil {
+								err = errors.New("Error: Cannot decode.")
+								return "", err
+							}
+							//	It was a private key
+							result = key
+
+							jsonBytes, err := json.Marshal(result)
+							if err != nil {
+								return "", err
+							}
+
+							return string(jsonBytes), nil
+						}
+					}
+					//	It was a certificate
+					result = cert
+
+					jsonBytes, err := json.Marshal(result)
+					if err != nil {
+						return "", err
+					}
+
+					return string(jsonBytes), nil
+				}
+			}
+			//	It was a CSR
+			result = csr
+
+			jsonBytes, err := json.Marshal(result)
+			if err != nil {
+				return "", err
+			}
+
+			return string(jsonBytes), nil
+		}
+	}
+
+	//	PEM decodes first time, so switch and decode for the specific type
+	switch block.Type {
+	case "CERTIFICATE REQUEST":
+		csr, err := x509.ParseCertificateRequest(block.Bytes)
+		if err != nil {
+			return "", err
+		}
+		result = csr
+	case "CERTIFICATE":
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return "", err
+		}
+		result = cert
+	case "PRIVATE KEY":
+		key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+		if err != nil {
+			return "", err
+		}
+		result = key
+	default:
+		return "", errors.New("unknown PEM type")
+	}
+
+	jsonBytes, err := json.Marshal(result)
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonBytes), nil
+}
